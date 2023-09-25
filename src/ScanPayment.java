5c4830c9151d07c18a3852b9fb6bebfc10d93fb1
naruapon <naruapon@gmail.com>
2023-09-25 16:21:25 +0700

    Change s to S

--- src/ScanPayment.java
+++ src/ScanPayment.java
@@ -3,7 +3,7 @@ public class ScanPayment implements PaymentStrategy{
 
 	@Override
 	public void pay(int amount) {
-        System.out.println("Paid by scan QR code: " + amount);
+        System.out.println("Paid by Scan QR code: " + amount);
 	}
 
 }
